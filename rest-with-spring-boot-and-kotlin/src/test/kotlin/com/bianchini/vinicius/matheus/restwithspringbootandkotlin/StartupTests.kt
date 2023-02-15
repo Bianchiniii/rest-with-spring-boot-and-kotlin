4d81c72aeb9650d1ac7099a448667e8458dae468
Matheus Vinicius Bianchini <mvbbianchini@hotmail.com>
2023-02-14 21:27:23 -0300

    feat: created project

---
+++ rest-with-spring-boot-and-kotlin/src/test/kotlin/com/bianchini/vinicius/matheus/restwithspringbootandkotlin/StartupTests.kt
@@ -0,0 +1,13 @@
+package com.bianchini.vinicius.matheus.restwithspringbootandkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StartupTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
